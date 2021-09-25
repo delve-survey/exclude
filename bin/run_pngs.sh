ee51a5791b190034c7da6d153e1effd13d5a1bf2
Alex Drlica-Wagner <kadrlica@fnal.gov>
2021-09-25 15:32:00 -0500

    create pngs for inspection

---
+++ bin/run_pngs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+urls=delve-urls-v1.csv 
+paths=delve-paths-v0.csv
+
+#for i in 9; do
+for i in $(seq 0 9); do
+    year=y${i}
+    filename=${year}/ghost-scatter-${year}.txt
+    log=${year}/run_pngs.log
+    csub -o $log python ./bin/pngs.py $filename $urls -p $paths -o ${year}/pngs
+done
